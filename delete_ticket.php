03701feb16b1378b15124047e9e1221e978958fb
ShounaKnk <shounakkannaik0607@gmail.com>
2023-11-22 16:42:57 +0530

    redirect of delete

--- delete_ticket.php
+++ delete_ticket.php
@@ -5,7 +5,7 @@
         $b_id=$_POST["b_id"];
         $q1="delete from tickets where b_id='$b_id'";
         $r1=mysqli_query($conn,$q1);
-        header("Location: HomePage.html");
+        header("Location: ticket_display.php");
         mysqli_close($conn);
     }
 ?>
